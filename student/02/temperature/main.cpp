b170b3908a308883a36f7b6a8491cd3665728e8e
Amy Hoang <my.hoang@tuni.fi>
2023-01-22 16:07:03 +0200

    modify the code

--- student/02/temperature/main.cpp
+++ student/02/temperature/main.cpp
@@ -4,9 +4,15 @@ using namespace std;
 
 int main()
 {
+    int x;
+    double y, z;
     cout << "Enter a temperature: ";
+    cin >> x;
 
-    // Write your code here
+    y = x * 1.8 + 32;
+    z = (x - 32) * 1.8;
+    cout << x <<" degrees Celsius is "<< y << " degrees Fahrenheit" <<endl;
+    cout << x <<" degrees Fahrenheit is "<< z << " degrees Celsius" <<endl;
 
     return 0;
 }
