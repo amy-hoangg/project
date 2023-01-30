e0c03314793e3d743be523909f45005451451795
Amy Hoang <my.hoang@tuni.fi>
2023-01-30 18:00:57 +0200

    random

--- student/03/random_numbers/main.cpp
+++ student/03/random_numbers/main.cpp
@@ -6,8 +6,23 @@ using namespace std;
 
 void produce_random_numbers(unsigned int lower, unsigned int upper)
 {
-    // Implement your function here
+    int seed;
+    cout << "Enter a seed value: ";
+    cin >> seed;
+
+    default_random_engine gen(seed);
+    uniform_int_distribution<int> distr(lower, upper);
+
+    string input = "c";
+    while (input != "q"){
+        cout << "Your drawn number is "<< distr(gen)<< endl;
+        cout << "Press q to quit or any other key to continue: ";
+        cin >> input;
+    }
+    return;
 }
+
+
 
 int main()
 {
