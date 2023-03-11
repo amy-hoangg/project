6bb0466214000ac8e8f1c5de6cd747ffbea304b6
Amy Hoang <my.hoang@tuni.fi>
2023-03-11 22:18:10 +0200

    implement class Library

--- student/07/library/library.cpp
+++ student/07/library/library.cpp
@@ -23,5 +23,36 @@ void Library::addBook(const string &author, const string &title, int reservation
 
 }
 
+vector<PairKeyValue> Library::flatDatabase() const
+{
+    vector<PairKeyValue> flatDatabase = {};
+    for(const auto &author : mapBooks_)//author is pair
+    {
+        for(const auto &book : author.second)
+        {
+            flatDatabase.push_back(make_pair(author.first,
+                                             book.title));
+            //book is not a string but a struct so have to extract title
+        }
+    }
+    return flatDatabase;
+}
+
+vector<PairKeyValue> Library::books_reservations(const string &author) const
+{
+    vector<PairKeyValue> books_reservations = {};
+    for(const auto &book : mapBooks_.at(author))
+    {
+        books_reservations.push_back(make_pair(book.title,
+                                     to_string(book.reservations)));
+    }
+    return books_reservations;
+}
+
+MapBooks Library::getMapBooks() const
+{
+    return mapBooks_;
+}
+
 
 
--- student/07/library/library.hh
+++ student/07/library/library.hh
@@ -27,11 +27,11 @@ class Library
 {
 public:
     // Constructor for Library.
-    // Param libraryName Name of the library.
+    // Param library: name of the library.
     Library(string const &libraryName);
 
     // Add author to the library's collection.
-    // Param author Author to add to the collection.
+    // Param author: author to add to the collection.
     void addAuthor(string const &author);
 
     // Add book to the library's collection.
@@ -41,6 +41,27 @@ public:
     void addBook(string const &author,
                  string const &title,
                  int reservations);
+
+    // Flatten library's book database by
+    //      iterates through the internal data structure of the library (mapBooks_),
+    //      and for each author and book combination
+    //      adds a new pair to a vector of pairs
+    // Return: a vector contains all books in the library with
+    //      their corresponding author names
+    vector<PairKeyValue> flatDatabase() const;
+
+    // Retrieve the list of books for the given author
+    //      from the internal data structure of the library (mapBooks_),
+    //      and for each book, adds a new pair to a vector of pairs,
+    //      returns a list of books by a given author, along with their reservation counts
+    // Param author: The name of the author for which to retrieve book information
+    // Return: a vector contains all books by the given author with
+    //      their corresponding reservation counts
+    vector<PairKeyValue> books_reservations(string const &author) const;
+
+    // Returns a copy of the library's internal book data structure (mapBooks_),
+    //      the structure is a map indexed by author name, with a vector of books for each author.
+    MapBooks getMapBooks () const;
 
 private:
     // Name of the library
