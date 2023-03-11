632351c33d702a07e77a7b5003e2d4fede75a7c6
Amy Hoang <my.hoang@tuni.fi>
2023-03-11 21:03:32 +0200

    init data structure, read file

--- student/07/library/library.hh
+++ student/07/library/library.hh
@@ -6,10 +6,15 @@
 #include <vector>
 
 using namespace std;
+
+// Forward declaration of Book to resolve circular dependency
 struct Book;
+
+// Define types for readability
 using MapBooks = map<string, vector<Book>>;
 using PairKeyValue = pair<string, string>;
 
+// Define Book struct
 struct Book
 {
     string author;
@@ -17,25 +22,31 @@ struct Book
     int reservations;
 };
 
+// Define Library class
 class Library
 {
 public:
+    // Constructor for Library.
+    // Param libraryName Name of the library.
     Library(string const &libraryName);
 
+    // Add author to the library's collection.
+    // Param author Author to add to the collection.
     void addAuthor(string const &author);
 
+    // Add book to the library's collection.
+    // Param author: author of the book.
+    // Param title: title of the book.
+    // Param reservations :number of reservations for the book.
     void addBook(string const &author,
-            string const &title,
-            int reservations);
-
-    vector<PairKeyValue> flatDatabase() const;
-
-    vector<PairKeyValue> books_reservations(string const &author) const;
-
-    MapBooks getMapBooks () const;
+                 string const &title,
+                 int reservations);
 
 private:
+    // Name of the library
     string libraryName_;
+
+    // Map of books, indexed by author name
     MapBooks mapBooks_;
 };
 
--- student/07/library/main.cpp
+++ student/07/library/main.cpp
@@ -7,18 +7,30 @@
 #include <string>
 #include <algorithm>
 #include <numeric>
-#include <cstdlib> //USE FOR EXIT
 #include <climits>
 
 using namespace std;
 using MapLibs = map<string, Library>;
 using PairKeyValue = pair<string, string>;
 
-vector<string> split(const string& s, const char delimiter, bool ignore_empty = false)
+//Function: split a string -> a vector of substrings
+//          based on delimiter
+//Param s: input string
+//Param delimiter: character used to split the string
+//Param ignore_empty: optional flag to indicate whether ignore
+//                    empty substrings in output vector or not
+//Return: a vector of substrings, split by the delimiter character
+
+vector<string> split(const string& s,
+                     const char delimiter,
+                     bool ignore_empty = false)
 {
     vector<string> result;
     string tmp = s;
 
+    // Loop through the input string,
+    // Split it into substrings at each occurrence of the delimiter character
+    // Add each non-empty substring to the output vector if the ignore_empty flag is not set
     while(tmp.find(delimiter) != string::npos)
     {
         string new_part = tmp.substr(0, tmp.find(delimiter));
@@ -35,36 +47,47 @@ vector<string> split(const string& s, const char delimiter, bool ignore_empty = 
     return result;
 }
 
+// Function: validate a line of input data to ensure it has correct format
+// Param line: the line of data to validate
+// Return: true if the line has the correct format, false otherwise
+
 bool isLineValid(string const &line)
 {
+    // Split the input line into parts using the ';' delimiter.
     vector<string> lineParts = split(line, ';');
 
+    // Check that there are exactly 4 fields in the input line.
     if (lineParts.size() != 4) {
         return false; // wrong number of fields
     }
 
+    // Check that each field in the input line is non-empty.
     for (auto const &part : lineParts) {
         if (part.empty()) {
             return false; // empty field
         }
     }
-
     return true;
 }
 
+// Function: Check if the input file is valid
+// Param filename: The name of the input file
+// Return: True if the file is valid, False otherwise
 bool isFileValid(string const &filename)
 {
-    //OPEN FILE
+    // Open the file
     ifstream file(filename);
 
-    //OPEN UNSUCCESSFULLY
+    // Check if the file could be opened
     if(!file)
     {
         cerr << "Error: input file cannot be opened" <<endl;
         return false;
     }
+
+    // Read each line from the file and check if it is valid
     string line;
-    while(getline(file, line)) //HAM GETLINE NO TU DONG LOOP LUON
+    while(getline(file, line))
     {
         if(!isLineValid(line))
         {
@@ -72,22 +95,27 @@ bool isFileValid(string const &filename)
             return false;
         }
     }
+    file.close();
     return true;
 }
 
-void readFile(string const &filename,
-              MapLibs &allLibraries)
+// Function: Read the input file and add books to the library system
+//
+// Param filename: The name of the input file
+// Param allLibraries: A Map of all the libraries in the system
+void readFile(string const &filename, MapLibs &allLibraries)
 {
-    //OPEN FILE
+    // Open the file
     ifstream file(filename);
 
+    // Read each line from the file and process it
     string line;
-    while(getline(file, line)) //HAM GETLINE NO TU DONG LOOP LUON
+    while(getline(file, line))
     {
-        //SPLIT
+        // Split the line into parts
         vector<string> lineParts = split(line, ';');
 
-        //WRITE OUT PARAMS
+        // Extract the parameters from the line
         string libraryName = lineParts.at(0);
         string author = lineParts.at(1);
         string title = lineParts.at(2);
@@ -97,340 +125,38 @@ void readFile(string const &filename,
             reservations = stoi(lineParts.at(3));
         }
 
-        //ADD LIB TO SYSTEM IF NOT AVAILABLE
+        // Add the library to the system if it is not already available
         if(allLibraries.find(libraryName) == allLibraries.end())
         {
             allLibraries.insert({libraryName, Library(libraryName)});
         }
 
-        //ADD BOOK, AUTHOR, RESERVE
+        // Add the book, author, and reservations to the library
         allLibraries.at(libraryName).addBook(author, title, reservations);
     }
     file.close();
 }
 
-void printLibraries(MapLibs const &allLibraries)
-{
-    for(MapLibs::const_iterator iter = allLibraries.begin();
-        iter != allLibraries.end();
-        iter++)
-    {
-        cout << iter->first << endl;
-    }
-}
-
-bool comparison(const PairKeyValue &pair1,
-                const PairKeyValue &pair2)
-{
-    if(pair1.first != pair2.first)
-    {
-        return pair1.first<pair2.first;
-    }
-    return pair1.second<pair2.second;
-}
-
-bool comparisonForReservations(const pair<string, int> &pair1,
-                              const pair<string, int> &pair2)
-{
-    if(pair1.second != pair2.second)
-    {
-        return pair1.second<pair2.second;
-    }
-    return pair1.first<pair2.first;
-}
-
-
-void printMaterial(Library const &libraryName)
-{
-    vector<PairKeyValue> flatLib = libraryName.flatDatabase();
-
-    //sort the data
-    sort(flatLib.begin(), flatLib.end(), comparison);
-
-    for(const auto &author_books : flatLib)
-    {
-        cout << author_books.first << ": " << author_books.second <<endl;
-    }
-}
-
-void printBooks(Library const &libraryName,
-                string const &authorName)
-{
-    vector<PairKeyValue> books_reservations = libraryName.books_reservations(authorName);
-    sort(books_reservations.begin(), books_reservations.end(), comparison);
-    for(const auto &books : books_reservations)
-    {
-        if(stoi(books.second) == 0)
-        {
-            cout << books.first << " --- " << "on the shelf" <<endl;
-        }
-        else
-        {
-            cout << books.first << " --- " << books.second <<" reservations "<<endl;
-        }
-    }
-}
-
-bool isOnTheShelf(vector<pair<string,int>> libs_reservations)
-{
-    for(const auto &pair : libs_reservations)
-    {
-        if(pair.second == 0)
-            return true;
-    }
-    return false;
-}
-
-void printReservable(MapLibs allLibraries,
-                     string const& reservableAuthorName,
-                     string const& reservableBookName)
-{
-    vector<pair<string, int>> libs_reservations;
-    int leastReservations = INT_MAX;
-    bool bookFound = false; //flag to know if the book is found or not
-
-    for (const auto& key_value_libs : allLibraries) {
-        MapBooks mapBooksGotten = key_value_libs.second.getMapBooks();
-        for (const auto& key_value_books : mapBooksGotten) {
-            vector<Book> vector_books = key_value_books.second;
-            for (Book bookFinding : vector_books)
-            {
-                if (bookFinding.author == reservableAuthorName &&
-                    bookFinding.title == reservableBookName) {
-                    bookFound = true;
-                    int current_reservations = bookFinding.reservations;
-                    if (current_reservations < leastReservations) {
-                        leastReservations = current_reservations;
-                        libs_reservations.clear();
-                        libs_reservations.push_back(make_pair(key_value_libs.first, leastReservations));
-                    } else if (current_reservations == leastReservations) {
-                        libs_reservations.push_back(make_pair(key_value_libs.first, leastReservations));
-                    }
-                }
-            }
-        }
-    }
-
-    //SORT NAME KEY_VALUE_LIBS
-    sort(libs_reservations.begin(), libs_reservations.end(), comparisonForReservations);
-
-    //NOT FOUND
-    if (!bookFound) {
-        cout << "Book is not a library book" << endl;
-        return; //EXIT THE FUNCTION
-    }
-
-    //CANNOT BE RESERVED
-    if (leastReservations == 100) {
-        cout << "Book is not reservable from any library" << endl;
-        return;
-    }
-
-    //CAN BE RESERVED
-    if (isOnTheShelf(libs_reservations) == true) {
-        cout << "on the shelf" << endl;
-    } else {
-        cout << leastReservations << " reservations" << endl;
-    }
-
-    for (const auto& pair : libs_reservations) {
-        cout << "--- " << pair.first << endl;
-    }
-}
-void printLoanable (MapLibs allLibraries)
-{
-    //USING SET TO KEEP TRACK, BUT NEED TO USE SORT
-    //SO HAVE TO STORE IN VECTOR INSTEAD OF SET
-    vector<PairKeyValue> author_book;
-    set<pair<string,string>> added_pairs;
-
-    for(const auto &key_value_libs : allLibraries)
-    {
-        MapBooks mapBooksGotten = key_value_libs.second.getMapBooks();
-        for(const auto &key_value_books : mapBooksGotten)
-        {
-            vector<Book> vector_books = key_value_books.second;
-            for(Book bookFinding : vector_books)
-            {
-                if(bookFinding.reservations == 0)
-                {
-                    PairKeyValue new_pair = make_pair(bookFinding.author,
-                                                      bookFinding.title);
-                    //The if (added_pairs.count(new_pair) == 0)
-                    //condition checks if the new pair is already
-                    //in the set added_pairs or not.
-                    if(added_pairs.count(new_pair) == 0)
-                    {
-                        author_book.push_back(new_pair);
-                        added_pairs.insert(new_pair);
-                    }
-                }
-            }
-        }
-    }
-    sort(author_book.begin(), author_book.end(), comparison);
-
-    for(const auto &pair : author_book)
-    {
-        cout << pair.first << ": " << pair.second <<endl;
-    }
-}
-
-
-string removeQuotes(string stringIncludeQuotes)
-{
-    if(stringIncludeQuotes.front() != '"'
-            && stringIncludeQuotes.back() != '"')
-    {
-        return stringIncludeQuotes;
-    }
-    size_t first = stringIncludeQuotes.find_first_not_of('"');
-    size_t last = stringIncludeQuotes.find_last_not_of('"');
-    string stringRemoveQuotes = stringIncludeQuotes.substr(first,
-                                                           last-first+1);
-    return stringRemoveQuotes;
-}
-
-bool isLibraryValid(MapLibs allLibraries,
-                      string const &libraryName)
-{
-    bool valid = false;
-    for(const auto &key_value_libs : allLibraries)
-    {
-        if(libraryName == key_value_libs.first)
-            valid = true;
-    }
-    if(valid == true) return true;
-
-    return false;
-}
-
-bool isAuthorValid(MapLibs allLibraries,
-                   string const &authorName)
-{
-    bool valid = false;
-    for(const auto &key_value_libs : allLibraries)
-    {
-        MapBooks mapBooksGotten = key_value_libs.second.getMapBooks();
-        for(const auto &key_value_books : mapBooksGotten)
-        {
-            if(authorName == key_value_books.first)
-                valid = true;
-        }
-    }
-    if(valid == true) return true;
-    return false;
-}
-
 int main()
 {
-    //INIT DATABASE
+    //initialize the data structures
     MapLibs allLibraries;
 
-    //READ FILENAME
+    //ask user to input filename
     string filename;
     cout << "Input file: ";
     getline(cin, filename);
 
-    //READ FILE AND PUT INTO DATABASE
+    //check if the file file is valid
+    //exit the program if the file is invalid
     if(isFileValid(filename) == false)
     {
         return EXIT_FAILURE;
     }
+
+    //read the file
+    //put the library data into database
     readFile(filename, allLibraries);
 
-
-    //************COMMANDS************
-    string userCommand;
-    while(true)
-    {
-        cout << "lib> ";
-        getline(cin, userCommand);
-
-        //SPLIT THE COMMAND
-        vector<string> command = split(userCommand, ' ');
-
-        //COMMAND = QUIT
-        if(userCommand == "quit")
-            break;
-
-        //COMMAND = LIBRARIES
-        else if(userCommand == "libraries")
-            printLibraries(allLibraries);
-
-        //COMMAND = MATERIAL
-        else if(command.at(0) == "material")
-        {
-            if(isLibraryValid(allLibraries, command.at(1)) == false)
-            {
-                cout << "Error: unknown library" <<endl;
-            }
-            else
-            {
-                string materialLibraryName = command.at(1);
-                printMaterial(allLibraries.at(materialLibraryName));
-            }
-        }
-        //COMMAND = BOOK LIB AUTHOR
-        else if(command.at(0) == "books")
-        {
-            //WRONG SIZE
-            if(command.size() != 3)
-            {
-                cout << "Error: wrong number of parameters" <<endl;
-            }
-
-            //WRONG LIBRARY
-            else if(isLibraryValid(allLibraries,command.at(1)) == false)
-            {
-                cout << "Error: unknown library" <<endl;
-            }
-
-            //TRUE LIBRARY, WRONG AUTHOR
-            else if(isAuthorValid(allLibraries, command.at(2)) == false)
-            {
-                cout << "Error: unknown author" <<endl;
-            }
-
-            else
-            {
-                string booksLibraryName = command.at(1);
-                string booksAuthorName = command.at(2);
-                printBooks(allLibraries.at(booksLibraryName), booksAuthorName);
-            }
-        }
-        
-        //COMMAND = RESERVABLE AUTHOR BOOK
-        else if(command.at(0) == "reservable")
-        {
-            string reservableAuthorName = command.at(1);
-            string reservableBookName;
-
-            for(auto iter_reservable = command.begin() + 2;
-                iter_reservable != command.end();
-                iter_reservable ++)
-            {
-                if(iter_reservable != command.begin()+2)
-                    reservableBookName += " ";
-                reservableBookName += *iter_reservable;
-            }
-            //TRUONG HOP CNO CO CAI QUOTES THI PHAI REMOVE
-            reservableBookName = removeQuotes(reservableBookName);
-
-            printReservable(allLibraries, reservableAuthorName, reservableBookName);
-        }
-
-        //COMMAND = LOANABLE
-        else if(command.at(0) == "loanable")
-        {
-            printLoanable(allLibraries);
-        }
-
-        //UNKNOWN COMMAND
-        else
-        {
-            cout << "Error: unknown command" <<endl;
-        }
-    }
     return EXIT_SUCCESS;
 }
