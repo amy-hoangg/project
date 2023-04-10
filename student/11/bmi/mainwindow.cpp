9b8565cedfbd8c0469defe1c92b72c0cdbfb3ad7
Amy Hoang <my.hoang@tuni.fi>
2023-04-10 15:24:48 +0300

    bmi

---
+++ student/11/bmi/mainwindow.cpp
@@ -0,0 +1,63 @@
+#include "mainwindow.hh"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    connect(ui->countButton,
+            &QPushButton::clicked,
+            this,
+            &MainWindow::on_countButton_clicked);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_countButton_clicked()
+{
+
+    height = ui->heightLineEdit->text().toFloat();
+    weight = ui->weightLineEdit->text().toFloat();
+
+    if(ui->heightLineEdit->text().isEmpty()
+            || height == 0)
+    {
+        ui->resultLabel->setText("Cannot count");
+
+    }
+    else
+    {
+        //tinh bmi
+        bmi = weight/(height*height)*10000;
+
+        QString bmiString = QString::number(bmi);
+        ui->resultLabel->setText(bmiString);
+
+        if(bmi<18.5)
+        {
+            ui->infoTextBrowser->setText("You are underweight.");
+        }
+
+        else if(18.5 <= bmi <= 25)
+        {
+            ui->infoTextBrowser->setText("Your weight is normal.");
+        }
+
+        else if(bmi>25)
+        {
+            ui->infoTextBrowser->setText("You are overweight.");
+        }
+    }
+}
+
+
+void MainWindow::on_closeButton_clicked()
+{
+    close();
+}
+
---
+++ student/11/bmi/mainwindow.hh
@@ -0,0 +1,30 @@
+#ifndef MAINWINDOW_HH
+#define MAINWINDOW_HH
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+
+    void on_countButton_clicked();
+
+    void on_closeButton_clicked();
+
+private:
+    Ui::MainWindow *ui;
+    float weight;
+    float height;
+    float bmi;
+};
+#endif // MAINWINDOW_HH
